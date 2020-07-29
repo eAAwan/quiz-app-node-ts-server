144d3dc879d174127b8f39f14ee9a24270985d9b
erum awan <eraw@danskebank.dk>
2020-07-29 21:16:35 +0200

    update port

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const constant_1 = __importDefault(require("./constant"));
 const app = express_1.default();
-const port = 3005;
+const port = process.env.port || 3000;
 app.get("/getquiz", (req, res) => {
     res.send(constant_1.default);
 });
-app.listen(port, (err) => {
-    if (err) {
-        return console.error(err);
-    }
+app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
 import quizQuestions from "./constant";
 const app = express();
-const port = 3005;
+const port = process.env.port || 3000;
 app.get("/getquiz", (req, res) => {
   res.send(quizQuestions);
 });
-app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
+app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
