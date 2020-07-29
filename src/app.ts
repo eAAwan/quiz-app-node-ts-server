c96f6a24d4c4a4498ecabefb5b513bd9ea2880f0
erum awan <eraw@danskebank.dk>
2020-07-29 21:26:37 +0200

    set dynamic port

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import quizQuestions from "./constant";
 const app = express();
-const port = process.env.port || 3000;
+
 app.get("/getquiz", (req, res) => {
   res.send(quizQuestions);
 });
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
