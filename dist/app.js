093a4a69c46feb63309553fec33c6fb57024c525
erum awan <eraw@danskebank.dk>
2020-07-29 20:34:41 +0200

    tsc error on herku

--- dist/app.js
+++ dist/app.js
@@ -6,21 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const constant_1 = __importDefault(require("./constant"));
 const app = express_1.default();
-const port = 3008;
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:" + port);
-    // Request methods you wish to allow
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    // Pass to next layer of middleware
-    next();
-});
+const port = 3005;
 app.get("/getquiz", (req, res) => {
     res.send(constant_1.default);
 });
